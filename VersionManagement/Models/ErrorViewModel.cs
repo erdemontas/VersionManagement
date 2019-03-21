95c7afa9e7ab7b05fcb5ccfba67485c53fadb1b2
Erdem ONTAS | PEAKUP <erdem.ontas@peakup.org>
2019-03-21 09:40:37 +0300

    Add project files.

---
+++ VersionManagement/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace VersionManagement.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
